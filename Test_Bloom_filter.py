d16d9690b81b9084955f2c26ec7560dbb854f9cd
Abdelrahman2799 <161724328+Abdelrahman2799@users.noreply.github.com>
2024-06-24 12:41:17 +0200

    Update Test_Bloom_filter.py
    Create generators for DNA and random language
    
    Add tests for hash functions with natural language and DNA sequence
    
    Print statistical analysis for hash functions
    
    Ensure uniqueness of tested words

--- Test_Bloom_filter.py
+++ Test_Bloom_filter.py
@@ -2,7 +2,9 @@ from Bloom_filter import BloomFilter
 from random import shuffle
 import random
 import string
+import mmh3
 from typing import List
+import numpy as np
 
 exp_count = 1000
 exp_fp_rate = 0.05
@@ -12,6 +14,10 @@ def get_words(ln: int) -> str:
     rndom = [random.choice(charachters) for _ in range(ln)]
     return "".join(rndom)
 
+def get_DNA(ln: int):
+    letters = 'ATCG'
+    return "".join(random.choice(letters) for _ in range(ln))
+
 def list_words(exp_count: int, words, min=3, max=12) -> List[str]:
     wlist = set()
     for i in range(exp_count):
@@ -19,6 +25,27 @@ def list_words(exp_count: int, words, min=3, max=12) -> List[str]:
         if string not in words:
             wlist.add(string)
     return list(wlist)
+
+def dna_generator(n, min=10, max=100):
+    return [get_DNA(random.randint(min, max)) for _ in range(n)]
+
+def words_generator(n, min=3, max=12):
+    return [get_words(random.randint(min, max)) for _ in range(n)]
+
+
+def test_hashF():
+    b = BloomFilter(exp_count, exp_fp_rate)
+    strings = words_generator(exp_count)
+    print("Hash functions test: natural language")
+    for i in range(b.hfunctions):
+        hashes = [mmh3.hash(string, i) % b.size for string in strings]
+        print(f'Function {i+1}: Minimum length = {min(hashes)}, Maximum length = {max(hashes)}, Mean={sum(hashes)/len(hashes)}, Standard deviation={np.std(hashes):.2f}')
+
+    print("Hash functions test: DNA sequences")
+    dna_seq = dna_generator(exp_count)
+    for i in range(b.hfunctions):
+        hashes = [mmh3.hash(dna, i) % b.size for dna in dna_seq]
+        print(f'Function {i+1}: Minimum length = {min(hashes)}, Maximum length = {max(hashes)}, Mean={sum(hashes)/len(hashes)}, Standard deviation={np.std(hashes):.2f}')
 
 def test_bf():
     words_to_insert = list_words(exp_count, set())
@@ -41,4 +68,3 @@ def test_bf():
     print(f'Actual false positive rate = {fp_prob:.2f}')
     if fp_prob > bf.fp_rate:
         print('Caution, the actual false positive rate exceeded the expected')
-    
