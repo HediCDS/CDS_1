7cc71d2fcfb98df0a3890a0542839f4da04bd9ff
Abdelrahman2799 <161724328+Abdelrahman2799@users.noreply.github.com>
2024-06-24 18:09:41 +0200

    Document Benchmark.py
    Add comments for clarity

--- Benchmark.py
+++ Benchmark.py
@@ -1,14 +1,17 @@
+#import necessary packages
 from Bloom_filter import BloomFilter
 import random
 import time
 import string
 import matplotlib.pyplot as plt
 
+#Function to provide lowercase strings of given length
 def get_words(ln: int) -> str:
     charachters = string.ascii_lowercase
     rndom = [random.choice(charachters) for _ in range(ln)]
     return "".join(rndom)
-
+    
+#Function to provide strings that are unique from the inserted ones, this to test false positives
 def list_words(exp_count: int, words, min=3, max=12) -> List[str]:
     wlist = set()
     for i in range(exp_count):
@@ -17,36 +20,60 @@ def list_words(exp_count: int, words, min=3, max=12) -> List[str]:
             wlist.add(string)
     return list(wlist)
 
+#Function to provide insertion times, search times and probabilities of false positives
 def benchmark(nlist, p):
     time_insert = []
     time_search = []
     prob_fpos = []
     for n in nlist:
-        bf = BloomFilter(n, p)    
+        #create a bloom filter
+        bf = BloomFilter(n, p)
+        
+        #get strings to be inserted
         strings_inserted = list_words(n, set())
+        
+        #mark the current time
         begin_time = time.time()
+        
+        #insert strings
         for string in strings_inserted:
             bf.insert(string)
+            
+        #mark insertion time
         time_ins = time.time() - begin_time
+        
+        #add the time to the insertion time list
         time_insert.append(time_ins)
+
+        #Provide a list og inserted and not inserted words
         strings_check_list = strings_inserted[:n // 2] + list_words(n // 2, set(strings_inserted)) 
+
+        #mark the time
         begin_search_time = time.time()
+
+        #set false positives counter to 0
         fp = 0
         for s in strings_check_list:
+            #search and track the fpositives
             if bf.search(s) and s not in strings_inserted:
                 fp += 1
+        #mark the search time and append to search time list
         check_time = time.time() - begin_search_time
         time_search.append(check_time)  
+
+        #Calculate the fp rate and append to the list of fp rates
         fprate = fp / (n // 2)
         prob_fpos.append(fprate)
     return time_insert, time_search, prob_fpos
 
-
+#Test with large samples and expected false pos rate
 nlist = [1000, 5000, 10000, 50000, 100000]
 fprate = 0.05
 
+#call the benchmark
 time_insert, time_search, prob_fp = benchmark(nlist, fprate)
 
+#plot the insertion time
 plt.figure(figsize=(14, 8))
 
 plt.subplot(1, 3, 1)
@@ -55,7 +82,7 @@ plt.xlabel('Words Counts')
 plt.ylabel('Time in seconds')
 plt.legend()
 
-
+#plot seach time
 plt.subplot(1, 3, 1)
 plt.plot(nlist, time_insert, label = "Insertion Time")
 plt.title('Insert Time')
@@ -63,7 +90,7 @@ plt.xlabel('Words Counts')
 plt.ylabel('Time in seconds')
 plt.legend()
 
-
+#plot fp probability
 plt.subplot(1, 3, 1)
 plt.plot(nlist, time_search, label = "Search Time")
 plt.title('Search Time')
