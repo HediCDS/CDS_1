b6cf43a4cc9a9e7872d07677ead357d5f7dcc396
HediCDS <162295896+HediCDS@users.noreply.github.com>
2024-06-24 11:41:07 +0200

    Modified Test_Bloom_filter.py
    Added test for fp rate
    
    Changed some variables names
    
    Modified Module name

--- Test_Bloom_filter.py
+++ Test_Bloom_filter.py
@@ -1,28 +1,28 @@
-from Testing import BloomFilter
+from Bloom_filter import BloomFilter
 from random import shuffle
 import random
 import string
 from typing import List
 
 exp_count = 1000
-fp_rate = 0.05
+exp_fp_rate = 0.05
 
 def get_words(ln: int) -> str:
     charachters = string.ascii_lowercase
     rndom = [random.choice(charachters) for _ in range(ln)]
     return "".join(rndom)
 
-def list_words(exp_count: int, words: List, min=3, max=12) -> List[str]:
-    list = set()
+def list_words(exp_count: int, words, min=3, max=12) -> List[str]:
+    wlist = set()
     for i in range(exp_count):
         string = get_words(random.randint(min, max))
         if string not in words:
-            list.add(string)
-    return list(list)
+            wlist.add(string)
+    return list(wlist)
 
 def test_bf():
-    words_to_insert = list_words(exp_count)
-    bf = BloomFilter(exp_count, fp_rate)
+    words_to_insert = list_words(exp_count, set())
+    bf = BloomFilter(exp_count, exp_fp_rate)
     for word in words_to_insert:
         bf.insert(word)
     shuffle(words_to_insert)
@@ -33,11 +33,12 @@ def test_bf():
     print("No false negatives")
     
     not_added = list_words(exp_count, words_to_insert)
-    for word in words_to_insert:
+    f_pos = 0
+    for word in not_added:
         if bf.search(word):
-            if word in not_added:
-                print(f'{word} is a false positive')
-            else:
-                print(f'{word} is probably present')
-        else:
-            print(f'{word} is not present')
+            f_pos += 1
+    fp_prob = f_pos / len(not_added)
+    print(f'Actual false positive rate = {fp_prob:.2f}')
+    if fp_prob > bf.fp_rate:
+        print('Caution, the actual false positive rate exceeded the expected')
+    
