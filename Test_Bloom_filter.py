76835aa1450db22a8dc3bcc2b192033fb87167a4
HediCDS <162295896+HediCDS@users.noreply.github.com>
2024-06-24 14:42:44 +0200

    Assert Test_Bloom_filter.py
    Add mean and std deviation assertions in hash function

--- Test_Bloom_filter.py
+++ Test_Bloom_filter.py
@@ -48,16 +48,24 @@ def test_hashF():
     print("Hash functions test: natural language")
     for i in range(b.hfunctions):
         hashes = [mmh3.hash(string, i) % b.size for string in strings]
+        Mean = np.mean(hashes)
+        StdDev = np.std(hashes) 
         #The dist of each hash function for natural language
         print(f'Function {i+1}: Minimum length = {min(hashes)}, Maximum length = {max(hashes)}, Mean={sum(hashes)/len(hashes)}, Standard deviation={np.std(hashes):.2f}')
+        assert Mean < 0.6 * b.size and Mean > 0.4 * b.size, 'Mean is outside the accepted range'
+        assert StdDev > 0, 'Standard Deviation must be positive'
 
     #Domain2: DNA sequences
     print("Hash functions test: DNA sequences")
     dna_seq = dna_generator(exp_count)
     for i in range(b.hfunctions):
         hashes = [mmh3.hash(dna, i) % b.size for dna in dna_seq]
+        Mean = np.mean(hashes)
+        StdDev = np.std(hashes)
         #The dist of hash functions for DNA sequences
         print(f'Function {i+1}: Minimum length = {min(hashes)}, Maximum length = {max(hashes)}, Mean={sum(hashes)/len(hashes)}, Standard deviation={np.std(hashes):.2f}')
+        assert Mean < 0.6 * b.size and Mean > 0.4 * b.size, 'Mean is outside the accepted range'
+        assert StdDev > 0, 'Standard Deviation must be positive'
 
 #Function to test false positives and false negatives
 def test_bf():
